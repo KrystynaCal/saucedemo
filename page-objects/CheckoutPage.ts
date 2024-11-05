233a971f367eb400c4c61401c0c85b45a63687d5
KrystynaCal <krystynacal5@gmail.com>
2024-11-05 22:03:47 +0100

    test: add checkout page and related tests for checkout functionality

---
+++ page-objects/CheckoutPage.ts
@@ -0,0 +1,31 @@
+import { Locator, Page } from "@playwright/test";
+
+export class CheckoutPage {
+  readonly page: Page;
+  readonly secondHeader: Locator;
+  readonly form: Locator;
+  readonly firstNameInput: Locator;
+  readonly lastNameInput: Locator;
+  readonly zipPostalCodeInput: Locator;
+  readonly continueButton: Locator;
+  readonly errorMessage: Locator;
+  readonly cancelButton: Locator;
+
+  constructor(page: Page) {
+    this.page = page;
+    this.secondHeader = page.locator('[data-test="title"]');
+    this.firstNameInput = page.locator('[data-test="firstName"]');
+    this.lastNameInput = page.locator('[data-test="lastName"]');
+    this.zipPostalCodeInput = page.locator('[data-test="postalCode"]');
+    this.continueButton = page.locator('[data-test="continue"]');
+    this.form = page.locator(".checkout_info");
+    this.errorMessage = page.locator('[data-test="error"]');
+    this.cancelButton = page.locator('[data-test="cancel"]');
+  }
+
+  async fillForm(firstName: string, lastName: string, zipPostalCode: string) {
+    await this.firstNameInput.fill(firstName);
+    await this.lastNameInput.fill(lastName);
+    await this.zipPostalCodeInput.fill(zipPostalCode);
+  }
+}
---
+++ tests/checkoutPage.test.ts
@@ -0,0 +1,56 @@
+import { expect, Page, test } from "@playwright/test";
+import { CartPage } from "../page-objects/CartPage";
+import { CheckoutPage } from "../page-objects/CheckoutPage";
+import { LoginPage } from "../page-objects/LoginPage";
+import { ProductPage } from "../page-objects/ProductPage";
+
+test.describe("Checkout Form Tests", () => {
+  let loginPage: LoginPage;
+  let productPage: ProductPage;
+  let cartPage: CartPage;
+  let checkoutPage: CheckoutPage;
+
+  test.beforeEach(async ({ page }) => {
+    loginPage = new LoginPage(page);
+    productPage = new ProductPage(page);
+    cartPage = new CartPage(page);
+    checkoutPage = new CheckoutPage(page);
+
+    await loginPage.navigate();
+    await loginPage.login("standard_user", "secret_sauce");
+  });
+
+  async function addProductsAndProceedToCheckout(page: Page) {
+    const products = ["Sauce Labs Bolt T-Shirt", "Sauce Labs Onesie"];
+    for (const product of products) {
+      await productPage.addToCartByProductName(product);
+    }
+    await productPage.navigateToCart();
+    await expect(cartPage.cartItems).toHaveCount(products.length);
+    await cartPage.proceedToCheckout();
+    await expect(page).toHaveURL(/checkout/);
+  }
+
+  test("Fill out checkout form and proceed", async ({ page }) => {
+    await addProductsAndProceedToCheckout(page);
+    await expect(checkoutPage.secondHeader).toBeVisible();
+    await expect(checkoutPage.form).toBeVisible();
+    await checkoutPage.fillForm("John", "Doe", "12345");
+    await checkoutPage.continueButton.click();
+  });
+
+  test("Check empty form validation", async ({ page }) => {
+    await addProductsAndProceedToCheckout(page);
+    await checkoutPage.continueButton.click();
+    await expect(checkoutPage.errorMessage).toBeVisible();
+    await expect(checkoutPage.errorMessage).toHaveText(
+      "Error: First Name is required"
+    );
+  });
+
+  test("Cancel button redirects to cart page", async ({ page }) => {
+    await addProductsAndProceedToCheckout(page);
+    await checkoutPage.cancelButton.click();
+    await expect(page).toHaveURL(/cart/);
+  });
+});
